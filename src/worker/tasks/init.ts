7471e402ec5c98933285adb341fb192b9ca653c9
ootr <git@ootr.jp>
2026-05-03 05:42:47 +0900

    remove basis encoder files and related code

--- src/worker/tasks/init.ts
+++ src/worker/tasks/init.ts
@@ -1,12 +1,10 @@
 import type { TypedWorkerWorker } from "@/_types/lib/worker/typedWorker";
-import { initPromise } from "@/lib/basis";
 import { setWorkerId } from "@/worker/env";
 
 export const registerInitHandler = (worker: TypedWorkerWorker) => {
-  worker.addEventListener("message", async (event) => {
+  worker.addEventListener("message", (event) => {
     if (event.data.type !== "init") return;
     setWorkerId(event.data.threadId);
-    await initPromise;
     worker.postMessage({
       type: "init",
       requestId: event.data.requestId,
